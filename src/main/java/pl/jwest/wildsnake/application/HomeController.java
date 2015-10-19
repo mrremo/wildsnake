f8dbf3fe683f0cf1a9bf41b005c5a9d9e482a039
remo <remix15@gmail.com>
2015-10-19 09:16:32 +0200

    Dodanie napisu na landing

--- src/main/java/pl/jwest/wildsnake/application/HomeController.java
+++ src/main/java/pl/jwest/wildsnake/application/HomeController.java
@@ -1,6 +1,8 @@
 package pl.jwest.wildsnake.application;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
+    @Value("${mainpage.message}")
+    private String pageMessage;
     @RequestMapping("/")
-    public String mainPage() { return "index";}
+    public String mainPage(Model model) {
+        model.addAttribute("pageMessage", pageMessage);
+        return "index";}
 }
